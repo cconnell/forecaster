96fdfda01340e6d29f1c6b508015a4335d6c0066
Chris Connell <chris.g.connell@gmail.com>
2017-03-26 12:33:16 -0500

    added dynamic styling to forecast day selector, changed day default to string to work with day selector

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -31,7 +31,7 @@ myApp.controller('mainController', ['$scope', 'cityFinder', function($scope, cit
 
 myApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityFinder', function($scope, $resource, $routeParams, cityFinder){
   $scope.city = cityFinder.city;
-  $scope.days = $routeParams.days || 2;
+  $scope.days = $routeParams.days || '2';
 
   $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=351f3fe1c21d085f91f5108c39b28909", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
   $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
